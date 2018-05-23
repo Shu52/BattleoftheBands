3a609feb4a1c946a796f75fa8fea5931186e28f4
Shuaib Sajid <shusajid@gmail.com>
2018-05-23 10:37:45 -0500

    file creation, broken battle of the bands exercise

---
+++ main.js
@@ -0,0 +1,15 @@
+const bandNumber = 1
+
+const takeNumber = function (bandName) {
+    
+   bandList = bandName;
+   return (`${bandNumber}. ${bandName}`);
+   bandNumber +=1;
+   console.log(bandNumber);
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
